4ba084ce4496bae5e4bb3eefdcf726a0d9c7c05b
Javier Rodríguez Parra <117043352+leroymerlinn@users.noreply.github.com>
2023-10-25 16:52:22 +0200

    Added a button that will be hidden and useless until some errors are fixed. The web icon has also changed.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let theme = document.getElementById("css");
+
 function clock() {
     today=new Date();
     m=today.getMinutes();
@@ -9,10 +10,44 @@ function clock() {
     {if (i<10) {i="0" + i;}return i;}
     window.onload=function(){clock();
 }
+
 let d = new Date();
 let h = d.getHours();
 if (h >= 20 || h <= 8) {
     theme.href = "style2.css";
 } else {
     theme.href = "style1.css";
-}	
+}
+
+/* function toggleUIMode() {
+    if (theme.getAttribute("href") == "style1.css") {
+        theme.href = "style2.css";
+        document.getElementById("button").innerHTML = "LIGHT MODE";
+    } else {
+        theme.href = "style1.css";
+        document.getElementById("button").innerHTML = "DARK MODE";
+    }
+}
+
+function storeCSS() {
+    localStorage.setItem("css", theme.getAttribute("href"));
+}
+function retrieveCSS() {
+    theme.href = localStorage.getItem("css");
+}
+
+function buttonName() {
+    if (theme.getAttribute("href") == "style1.css") {
+        document.getElementById("button").innerHTML = "DARK MODE"
+    } else {
+        document.getElementById("button").innerHTML = "LIGHT MODE"
+    }
+}
+
+window.onload = function() {
+    setTimeout(loadPage, 500);
+}
+
+function loadPage() {
+    document.getElementsByTagName("body").style.visibility = "visible";
+} */
