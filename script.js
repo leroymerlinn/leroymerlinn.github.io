113e6ce145d87d914c37be0ac4a6eebbc50fd6f4
Javier Rodríguez Parra <117043352+leroymerlinn@users.noreply.github.com>
2023-10-30 10:04:42 +0100

    Reduced unnecessary code.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let theme = document.getElementById("css");
-
 function clock() {
     today=new Date();
     m=today.getMinutes();
@@ -10,44 +8,12 @@ function clock() {
     {if (i<10) {i="0" + i;}return i;}
     window.onload=function(){clock();
 }
-
+let theme = document.getElementById("css");
 let d = new Date();
 let h = d.getHours();
-if (h >= 20 || h <= 8) {
+let m = d.getMinutes();
+if (h >= 18 && m >= 8 || h <= 7 && m <= 30) {
     theme.href = "style2.css";
 } else {
     theme.href = "style1.css";
 }
-
-/* function toggleUIMode() {
-    if (theme.getAttribute("href") == "style1.css") {
-        theme.href = "style2.css";
-        document.getElementById("button").innerHTML = "LIGHT MODE";
-    } else {
-        theme.href = "style1.css";
-        document.getElementById("button").innerHTML = "DARK MODE";
-    }
-}
-
-function storeCSS() {
-    localStorage.setItem("css", theme.getAttribute("href"));
-}
-function retrieveCSS() {
-    theme.href = localStorage.getItem("css");
-}
-
-function buttonName() {
-    if (theme.getAttribute("href") == "style1.css") {
-        document.getElementById("button").innerHTML = "DARK MODE"
-    } else {
-        document.getElementById("button").innerHTML = "LIGHT MODE"
-    }
-}
-
-window.onload = function() {
-    setTimeout(loadPage, 500);
-}
-
-function loadPage() {
-    document.getElementsByTagName("body").style.visibility = "visible";
-} */
